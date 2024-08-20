7d6670472f86c14768d5329802d1418723e6cccf
arjun-esguerra <arjun.esguerra@hmcarchitects.com>
2024-08-20 14:50:06 -0700

    initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Phish_Sweep;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
